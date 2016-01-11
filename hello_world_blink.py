8805dfc43f7bef196fc49b754382a0e71797b64d
Loc Trinh <loctrinh@mit.edu>
2016-01-11 10:36:48 -0500

    Initial commit

---
+++ hello_world_blink.py
@@ -0,0 +1,20 @@
+from driver.tamproxy import Sketch, SyncedSketch, Timer
+from driver.tamproxy.devices import DigitalOutput
+
+
+class Blink(Sketch):
+
+    def setup(self):
+        self.led = DigitalOutput(self.tamp, 13)
+        self.led_timer = Timer()
+        self.led_state = False
+
+    def loop(self):
+        if self.led_timer.millis() > 1000:
+            self.led_timer.reset()
+            self.led_state = not self.led_state
+            self.led.write(self.led_state)
+
+if __name__ == "__main__":
+    sketch = Blink()
+    sketch.run()
