de02c162be5d252538d11506229f85a609b9b313
mfeffer <mfeffer@mit.edu>
2016-01-13 13:22:55 -0500

    Cleaned up map processing code (may add build option later to toggle between red and green).

--- map_processing.py
+++ map_processing.py
@@ -10,7 +10,6 @@ for line in map_input:
 	line = line.strip()
 	elements = line.split(",")
 	elements = map(get_int,elements)
-	print elements
 	
 	lines.append(elements)
 	for elt in elements:
@@ -18,7 +17,6 @@ for line in map_input:
 			max_loc = max(max_loc,elt)
 grid = [['-' for x in range(max_loc+1)] for x in range(max_loc+1)]
 map_input.close()
-print len(grid)
 # Wall (W)
 # Platform (P)
 # Red Cube (R)
@@ -26,6 +24,7 @@ print len(grid)
 # Stack of Cubes (S)
 # Home Base (H) (DOESN'T MATTER)
 # Start Location (L)
+
 for read_line in lines:
 	if(read_line[0] == 'W'):
 		for x in xrange(2,len(read_line),+2):
@@ -35,214 +34,18 @@ for read_line in lines:
 		for x in xrange(2,len(read_line),+2):
 			grid[int(read_line[x])][int(read_line[x-1])] = 'W'
 	elif(read_line[0] == 'S'):
-		grid[int(read_line[2])][int(read_line[1])] = read_line[3] #not caring about stacks right now...
-	# elif(read_line[0] == 'H'):
-	# 	for x in xrange(3,len(read_line),+2):
-	# 		grid[int(read_line[x-1])-1][int(read_line[x])-1] = 'H'	
-	# 		print read_line[x-1]
-	# 		print read_line[x]
-	#LOLNOPE
+		grid[int(read_line[2])][int(read_line[1])] = read_line[3] 
 
 	elif(read_line[0] == 'L'):
 		grid[int(read_line[2])][int(read_line[1])] = 'L'
 
-#print grid
+# out = open("map_output3.txt",'w')
+# for line in grid:
+# 	out.write(" ".join(line))
+# 	out.write("\r\n")
+# out.close()
 
-out = open("map_output3.txt",'w')
-for line in grid:
-	out.write(" ".join(line))
-	out.write("\r\n")
-out.close()
-
-grid2 = [['-' for x in range(3*max_loc)] for x in range(3*max_loc)]
-
-
-for read_line in lines:
-	if(read_line[0] == 'W'):
-		x_result = read_line[1] - read_line[3]
-		y_result = read_line[2] - read_line[4]
-		if(x_result == 0 and y_result == 1):
-			new_x = read_line[1] * 3
-			start_y = read_line[2] * 3
-			end_y = read_line[4] * 3
-			while(start_y >= end_y):
-				grid2[start_y][new_x] = 'W'
-				start_y -= 1
-		elif(x_result == 0 and y_result == -1):
-			new_x = read_line[1] * 3
-			start_y = read_line[2] * 3
-			end_y = read_line[4] * 3
-			while(start_y <= end_y):
-				grid2[start_y][new_x] = 'W'
-				start_y += 1
-
-		elif(x_result == 1 and y_result == 0):
-			new_y = read_line[2] * 3
-			start_x = read_line[1] * 3
-			end_x = read_line[3] * 3
-			while(start_x >= end_x):
-				grid2[new_y][start_x] = "W"
-				start_x -= 1
-
-		elif(x_result == -1 and y_result == 0):
-			new_y = read_line[2] * 3
-			start_x = read_line[1] * 3
-			end_x = read_line[3] * 3
-			while(start_x <= end_x):
-				grid2[new_y][start_x] = "W"
-				start_x += 1			
-
-		elif(x_result == 1 and y_result == 1):
-			start_x = read_line[1] * 3
-			start_y = read_line[2] * 3
-			end_x = read_line[3] * 3
-			end_y = read_line[4] * 3
-			while start_x > end_x and start_y > end_y:
-				grid2[start_y][start_x] = 'W'
-				start_x -= 1
-				start_y -= 1
-
-		elif(x_result == 1 and y_result == -1):
-			start_x = read_line[1] * 3
-			start_y = read_line[2] * 3
-			end_x = read_line[3] * 3
-			end_y = read_line[4] * 3
-			while start_x > end_x and start_y < end_y:
-				grid2[start_y][start_x] = 'W'
-				start_x -= 1
-				start_y += 1
-
-		elif(x_result == -1 and y_result == 1):
-			start_x = read_line[1] * 3
-			start_y = read_line[2] * 3
-			end_x = read_line[3] * 3
-			end_y = read_line[4] * 3
-			while start_x < end_x and start_y < end_y:
-				grid2[start_y][start_x] = 'W'
-				start_x += 1
-				start_y -= 1
-
-		elif(x_result == -1 and y_result == -1):
-			start_x = read_line[1] * 3
-			start_y = read_line[2] * 3
-			end_x = read_line[3] * 3
-			end_y = read_line[4] * 3
-			# print "Y HERE"
-			# print read_line[2]
-			# print read_line[4]
-			while start_x < end_x and start_y < end_y:
-				grid2[start_y][start_x] = 'W'
-				start_x += 1
-				start_y += 1
-				print start_x
-				print start_y
-
-		# for x in xrange(2,len(read_line),+2):
-		# 	grid[int(read_line[x-1])-1][int(read_line[x])-1] = 'W'
-
-	elif(read_line[0] == 'P'):
-
-		x_result = read_line[1] - read_line[3]
-		y_result = read_line[2] - read_line[4]
-		if(x_result == 0 and y_result == 1):
-			new_x = read_line[1] * 3
-			start_y = read_line[2] * 3
-			end_y = read_line[4] * 3
-			while(start_y >= end_y):
-				grid2[start_y][new_x] = 'W'
-				start_y -= 1
-		elif(x_result == 0 and y_result == -1):
-			new_x = read_line[1] * 3
-			start_y = read_line[2] * 3
-			end_y = read_line[4] * 3
-			while(start_y <= end_y):
-				grid2[start_y][new_x] = 'W'
-				start_y += 1
-
-		elif(x_result == 1 and y_result == 0):
-			new_y = read_line[2] * 3
-			start_x = read_line[1] * 3
-			end_x = read_line[3] * 3
-			while(start_x >= end_x):
-				grid2[new_y][start_x] = 'W'
-				start_x -= 1
-
-		elif(x_result == -1 and y_result == 0):
-			new_y = read_line[2] * 3
-			start_x = read_line[1] * 3
-			end_x = read_line[3] * 3
-			while(start_x <= end_x):
-				grid2[new_y][start_x] = 'W'
-				start_x += 1			
-
-		elif(x_result == 1 and y_result == 1):
-			start_x = read_line[1] * 3
-			start_y = read_line[2] * 3
-			end_x = read_line[3] * 3
-			end_y = read_line[4] * 3
-			while start_x > end_x and start_y > end_y:
-				grid2[start_y][start_x] = 'W'
-				start_x -= 1
-				start_y -= 1
-
-		elif(x_result == 1 and y_result == -1):
-			start_x = read_line[1] * 3
-			start_y = read_line[2] * 3
-			end_x = read_line[3] * 3
-			end_y = read_line[4] * 3
-			while start_x > end_x and start_y < end_y:
-				grid2[start_y][start_x] = 'W'
-				start_x -= 1
-				start_y += 1
-
-		elif(x_result == -1 and y_result == 1):
-			start_x = read_line[1] * 3
-			start_y = read_line[2] * 3
-			end_x = read_line[3] * 3
-			end_y = read_line[4] * 3
-			while start_x < end_x and start_y < end_y:
-				grid2[start_y][start_x] = 'W'
-				start_x += 1
-				start_y -= 1
-
-		elif(x_result == -1 and y_result == -1):
-			start_x = read_line[1] * 3
-			start_y = read_line[2] * 3
-			end_x = read_line[3] * 3
-			end_y = read_line[4] * 3
-			# print "Y HERE"
-			# print read_line[2]
-			# print read_line[4]
-			while start_x < end_x and start_y < end_y:
-				grid2[start_y][start_x] = 'W'
-				start_x += 1
-				start_y += 1
-				print start_x
-				print start_y
-		
-		# for x in xrange(2,len(read_line),+2):
-		# 	grid2[int(read_line[x-1])-1][int(read_line[x])-1] = 'P'
-		
-	elif(read_line[0] == 'S'):
-		grid2[3*(read_line[2])][3*(read_line[1])] = read_line[3] #not caring about stacks right now...
-		
-	# elif(read_line[0] == 'H'):
-	# 	for x in xrange(3,len(read_line),+2):
-	# 		grid[int(read_line[x-1])-1][int(read_line[x])-1] = 'H'	
-	# 		print read_line[x-1]
-	# 		print read_line[x]
-	#LOLNOPE
-
-	elif(read_line[0] == 'L'):
-		grid2[3*(read_line[1])-1][3*(read_line[2])-1] = 'L'
-		
-out2 = open("map_output2.txt",'w')
-for line in grid2:
-	out2.write(" ".join(line))
-	out2.write("\r\n")
-out2.close()
-
-grid3 = [['-' for x in range(3*(max_loc+1))] for x in range(3*(max_loc+1))]
+large_grid = [['-' for x in range(3*(max_loc+1))] for x in range(3*(max_loc+1))]
 
 
 
@@ -250,24 +53,20 @@ grid3 = [['-' for x in range(3*(max_loc+1))] for x in range(3*(max_loc+1))]
 for row in xrange((max_loc+1)):
 	for col in xrange((max_loc+1)):
 		if(grid[row][col] == 'W'):
-			print "here"
 			if(row < len(grid) - 1 and grid[row+1][col] == 'W'):
-				print "case 1"
 				start_row = row * 3 + 1
 				end_row = (row+1) * 3 + 1
 				new_col = col * 3 + 1
 				while(start_row <= end_row):
-					print "maybe here?"
-					grid3[start_row][new_col] = 'W'
+					large_grid[start_row][new_col] = 'W'
 					start_row += 1
 
 			if(row > 0 and grid[row-1][col] == 'W'):
-				print "case 2"
 				start_row = row * 3 + 1
 				end_row = (row-1) * 3 + 1
 				new_col = col * 3 + 1
 				while(start_row <= end_row):
-					grid3[start_row][new_col] = 'W'
+					large_grid[start_row][new_col] = 'W'
 					start_row -= 1
 
 			if(col > 0 and grid[row][col-1] == 'W'):
@@ -275,7 +74,7 @@ for row in xrange((max_loc+1)):
 				end_col = (col-1) * 3 + 1
 				new_row = row * 3 + 1
 				while(start_col >= end_col):
-					grid3[new_row][start_col] = 'W'
+					large_grid[new_row][start_col] = 'W'
 					start_col -= 1
 
 			if(col < len(grid) - 1 and grid[row][col+1] == 'W'):
@@ -283,7 +82,7 @@ for row in xrange((max_loc+1)):
 				end_col = (col+1) * 3 + 1
 				new_row = row * 3 + 1
 				while(start_col <= end_col):
-					grid3[new_row][start_col] = 'W'
+					large_grid[new_row][start_col] = 'W'
 					start_col += 1
 
 			if(row < len(grid) - 1 and col < len(grid) - 1 and grid[row+1][col+1] == 'W' and grid[row+1][col] != 'W' and grid[row][col+1] != 'W'):
@@ -292,7 +91,7 @@ for row in xrange((max_loc+1)):
 				start_col = col * 3 + 1
 				end_col = (col+1) * 3 + 1
 				while(start_row <= end_row and start_col <= end_col):
-					grid3[start_row][start_col] = 'W'
+					large_grid[start_row][start_col] = 'W'
 					start_row += 1
 					start_col += 1
 
@@ -302,7 +101,7 @@ for row in xrange((max_loc+1)):
 				start_col = col * 3 + 1
 				end_col = (col-1) * 3 + 1
 				while(start_row >= end_row and start_col >= end_col):
-					grid3[start_row][start_col] = 'W'
+					large_grid[start_row][start_col] = 'W'
 					start_row -= 1
 					start_col -= 1			
 
@@ -312,119 +111,31 @@ for row in xrange((max_loc+1)):
 				start_col = col * 3 + 1
 				end_col = (col+1) * 3 + 1
 				while(start_row >= end_row and start_col <= end_col):
-					grid3[start_row][start_col] = 'W'
+					large_grid[start_row][start_col] = 'W'
 					start_row -= 1
 					start_col += 1
+
 			if(row < len(grid) - 1 and col > 0 and grid[row+1][col-1] == 'W' and grid[row+1][col] != 'W' and grid[row][col-1] != 'W'):
 				start_row = row * 3 + 1
 				end_row = (row+1) * 3 + 1
 				start_col = col * 3 + 1
 				end_col = (col-1) * 3 + 1
 				while(start_row <= end_row and start_col >= end_col):
-					grid3[start_row][start_col] = 'W'
+					large_grid[start_row][start_col] = 'W'
 					start_row += 1
 					start_col -= 1
-
-		# x_result = read_line[1] - read_line[3]
-		# y_result = read_line[2] - read_line[4]
-		# if(x_result == 0 and y_result == 1):
-		# 	new_x = read_line[1] * 3 + 1
-		# 	start_y = read_line[2] * 3 + 1
-		# 	end_y = read_line[4] * 3 + 1
-		# 	while(start_y >= end_y):
-		# 		grid3[start_y][new_x] = 'W'
-		# 		start_y -= 1
-		# elif(x_result == 0 and y_result == -1):
-		# 	new_x = read_line[1] * 3 + 1
-		# 	start_y = read_line[2] * 3 + 1
-		# 	end_y = read_line[4] * 3 + 1
-		# 	while(start_y <= end_y):
-		# 		grid3[start_y][new_x] = 'W'
-		# 		start_y += 1
-
-		# elif(x_result == 1 and y_result == 0):
-		# 	new_y = read_line[2] * 3 + 1
-		# 	start_x = read_line[1] * 3 + 1
-		# 	end_x = read_line[3] * 3 + 1
-		# 	while(start_x >= end_x):
-		# 		grid3[new_y][start_x] = "W"
-		# 		start_x -= 1
-
-		# elif(x_result == -1 and y_result == 0):
-		# 	new_y = read_line[2] * 3 + 1
-		# 	start_x = read_line[1] * 3 + 1
-		# 	end_x = read_line[3] * 3 + 1
-		# 	while(start_x <= end_x):
-		# 		grid3[new_y][start_x] = "W"
-		# 		start_x += 1			
-
-		# elif(x_result == 1 and y_result == 1):
-		# 	start_x = read_line[1] * 3 + 1
-		# 	start_y = read_line[2] * 3 + 1
-		# 	end_x = read_line[3] * 3 + 1
-		# 	end_y = read_line[4] * 3 + 1
-		# 	while start_x > end_x and start_y > end_y:
-		# 		grid3[start_y][start_x] = 'W'
-		# 		start_x -= 1
-		# 		start_y -= 1
-
-		# elif(x_result == 1 and y_result == -1):
-		# 	start_x = read_line[1] * 3 + 1
-		# 	start_y = read_line[2] * 3 + 1
-		# 	end_x = read_line[3] * 3 + 1
-		# 	end_y = read_line[4] * 3 + 1
-		# 	while start_x > end_x and start_y < end_y:
-		# 		grid3[start_y][start_x] = 'W'
-		# 		start_x -= 1
-		# 		start_y += 1
-
-		# elif(x_result == -1 and y_result == 1):
-		# 	start_x = read_line[1] * 3 + 1
-		# 	start_y = read_line[2] * 3 + 1
-		# 	end_x = read_line[3] * 3 + 1
-		# 	end_y = read_line[4] * 3 + 1
-		# 	while start_x < end_x and start_y < end_y:
-		# 		grid3[start_y][start_x] = 'W'
-		# 		start_x += 1
-		# 		start_y -= 1
-
-		# elif(x_result == -1 and y_result == -1):
-		# 	start_x = read_line[1] * 3 + 1
-		# 	start_y = read_line[2] * 3 + 1
-		# 	end_x = read_line[3] * 3 + 1
-		# 	end_y = read_line[4] * 3 + 1
-		# 	# print "Y HERE"
-		# 	# print read_line[2]
-		# 	# print read_line[4]
-		# 	while start_x < end_x and start_y < end_y:
-		# 		grid3[start_y][start_x] = 'W'
-		# 		start_x += 1
-		# 		start_y += 1
-		# 		print start_x
-		# 		print start_y
-
-		# # for x in xrange(2,len(read_line),+2):
-		# 	grid[int(read_line[x-1])-1][int(read_line[x])-1] = 'W'
-
 			
 		elif(grid[row][col] == 'R'):
-			grid3[3*(row) + 1][3*(col) + 1] = 'R'
+			large_grid[3*(row) + 1][3*(col) + 1] = 'R'
 		elif(grid[row][col] == 'G'):
-			grid3[3*(row) + 1][3*(col) + 1] = 'G'
-			
-		# elif(read_line[0] == 'H'):
-		# 	for x in xrange(3,len(read_line),+2):
-		# 		grid[int(read_line[x-1])-1][int(read_line[x])-1] = 'H'	
-		# 		print read_line[x-1]
-		# 		print read_line[x]
-		#LOLNOPE
+			large_grid[3*(row) + 1][3*(col) + 1] = 'G'
 
 		elif(grid[row][col] == 'L'):
-			grid3[3*row + 1][3*col + 1] = 'L'
+			large_grid[3*row + 1][3*col + 1] = 'L'
 
 
-out4 = open("map_output4.txt",'w')
-for line in grid3:
-	out4.write(" ".join(line))
-	out4.write("\r\n")
-out4.close()
+out = open("large_map_output.txt",'w')
+for line in large_grid:
+	out.write(" ".join(line))
+	out.write("\r\n")
+out.close()
